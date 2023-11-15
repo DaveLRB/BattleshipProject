7f4389a1bc06929be1351f1df304f3d0f6d4cd18
DaveLRB <148245690+DaveLRB@users.noreply.github.com>
2023-11-15 17:47:30 +0000

    submarine added

--- BattleshipGames/src/AircraftCarrier.java
+++ BattleshipGames/src/AircraftCarrier.java
@@ -1,2 +1,21 @@
-public class AircraftCarrier{
+public class AircraftCarrier extends Ship{
+
+    public void placeShipHorizontally(String[][] boardgame, Coordinate coordinate) {
+
+    }
+
+
+    public void placeShipVertically(String[][] boardgame, Coordinate coordinate) {
+
+    }
+
+
+    public boolean isPlacingShipHorizontallyPossible(String[][] boardgame, Coordinate coordinate) {
+        return false;
+    }
+
+
+    public boolean isPlacingShipVerticallyPossible(String[][] boardgame, Coordinate coordinate) {
+        return false;
+    }
 }
