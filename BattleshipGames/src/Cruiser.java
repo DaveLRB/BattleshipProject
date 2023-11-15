87d085db7df6ee61ec34f5bc2f9a6cb9fd7c578b
DaveLRB <148245690+DaveLRB@users.noreply.github.com>
2023-11-15 17:17:09 +0000

    destroyer and cruiser added

--- BattleshipGames/src/Cruiser.java
+++ BattleshipGames/src/Cruiser.java
@@ -1,2 +1,38 @@
-public class Cruiser{
+public class Cruiser extends Ship{
+
+    public void placeShipHorizontally(String[][] boardgame, Coordinate coordinate) {
+        boardgame[coordinate.getRow()][coordinate.getColumn()] = boardgame[coordinate.getRow()][coordinate.getColumn()].replace(WATER_SYMBOL, SHIP_SYMBOL);
+        boardgame[coordinate.getRow()][coordinate.getColumn() - 1] = boardgame[coordinate.getRow()][coordinate.getColumn()].replace(WATER_SYMBOL, SHIP_SYMBOL);
+        boardgame[coordinate.getRow()][coordinate.getColumn() + 1] = boardgame[coordinate.getRow()][coordinate.getColumn()].replace(WATER_SYMBOL, SHIP_SYMBOL);
+    }
+
+    public void placeShipVertically(String[][] boardgame, Coordinate coordinate) {
+        boardgame[coordinate.getRow()][coordinate.getColumn()] = boardgame[coordinate.getRow()][coordinate.getColumn()].replace(WATER_SYMBOL, SHIP_SYMBOL);
+        boardgame[coordinate.getRow() - 1][coordinate.getColumn()] = boardgame[coordinate.getRow()][coordinate.getColumn()].replace(WATER_SYMBOL, SHIP_SYMBOL);
+        boardgame[coordinate.getRow() + 1][coordinate.getColumn()] = boardgame[coordinate.getRow()][coordinate.getColumn()].replace(WATER_SYMBOL, SHIP_SYMBOL);
+    }
+
+
+    public boolean isPlacingShipHorizontallyPossible(String[][] boardgame, Coordinate coordinate) {
+        try {
+            if (boardgame[coordinate.getRow()][coordinate.getColumn()].equals(WATER_SYMBOL) && boardgame[coordinate.getRow()][coordinate.getColumn() - 1].equals(WATER_SYMBOL) && boardgame[coordinate.getRow()][coordinate.getColumn() + 1].equals(WATER_SYMBOL)) {
+                return true;
+            }
+        } catch (Exception error) {
+            return false;
+        }
+        return false;
+    }
+
+
+    public boolean isPlacingShipVerticallyPossible(String[][] boardgame, Coordinate coordinate) {
+        try {
+            if (boardgame[coordinate.getRow()][coordinate.getColumn()].equals(WATER_SYMBOL) && boardgame[coordinate.getRow() - 1][coordinate.getColumn()].equals(WATER_SYMBOL) && boardgame[coordinate.getRow() + 1][coordinate.getColumn()].equals(WATER_SYMBOL)) {
+                return true;
+            }
+        } catch (Exception error) {
+            return false;
+        }
+        return false;
+    }
 }
