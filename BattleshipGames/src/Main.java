a2254b5c88dbf822be9075357b130b7df7d88370
Fábio <fabio.goncalves@school.mindera.com>
2023-11-16 18:56:54 +0000

    little improvements on the main class

--- BattleshipGames/src/Main.java
+++ BattleshipGames/src/Main.java
@@ -1,31 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
+    private static final String PLAYER_VS_PLAYER = "1";
+    private static final String PLAYER_VS_COMPUTER = "2";
+    private static final String EXIT_GAME = "0";
+
     public static void main(String[] args) {
         Player player1 = new Player();
         Player player2 = new Player();
         GameManager game = new GameManager();
 
         //Scanner input = new Scanner(System.in);
-        System.out.println("  +++++ BATTLESHIP GAME +++++");
-        System.out.println("   +++++  GET STARTED  +++++");
-        System.out.println("     +++++ GAME MODE +++++ \n");
-        System.out.println("     1. Player VS Player");
-        System.out.println("     2. Player VS Computer");
-        System.out.println("           0. Exit");
-        System.out.println();
+        printGameMainMenu();
         Scanner input = new Scanner(System.in);
-        String options = input.nextLine();
-        switch (options) {
-            case "1":
-                playerPlay(player1,game, "1");
-                playerPlay(player2,game,"2");
+        String selectedOption = input.nextLine();
+        switch (selectedOption) {
+            case PLAYER_VS_PLAYER:
+                playerPlay(player1, game, "1");
+                playerPlay(player2, game, "2");
                 //game.startPlayerVsPlayerGame();
                 break;
-            case "2":
+            case PLAYER_VS_COMPUTER:
 
                 break;
-            case "0":
+            case EXIT_GAME:
                 System.out.println("Exiting Game!");
                 input.close();
                 System.exit(0);
@@ -37,32 +35,45 @@ public class Main {
 
     }
 
-    private static void playerPlay(Player player, GameManager game, String numberOfPlayer){
-        System.out.println("\nIt's the player"+numberOfPlayer+" time to choose\n");
+    private static void playerPlay(Player player, GameManager game, String numberOfPlayer) {
+        System.out.println("\nIt's the player" + numberOfPlayer + " time to choose\n");
         System.out.println("1. Place Ships Manually\n2. Place Ships Randomly");
         Scanner scan = new Scanner(System.in);
         String userInputPlayer = scan.nextLine();
-        switch(userInputPlayer){
+        switch (userInputPlayer) {
             case "1":
-                System.out.println("\nIt's the player"+numberOfPlayer+" time to choose\n");
+                System.out.println("\nIt's the player" + numberOfPlayer + " time to choose\n");
                 game.placeShipsManually(player);
                 break;
             case "2":
                 game.placeShipsRandomly(player);
                 String choiceInput;
-                do{
+                do {
                     System.out.println("\n1. Continue\n2. Redo randomizer\n");
                     //input.nextLine();
                     choiceInput = scan.nextLine();
-                    if(!(choiceInput.equals("1"))){
+                    if (!(choiceInput.equals("1"))) {
                         game.placeShipsRandomly(player);
                     }
-                }while(!(choiceInput.equals("1")));
+                } while (!(choiceInput.equals("1")));
                 break;
             default:
                 System.out.println("please input correct one");
                 break;
         }
     }
+
+    private static void printGameMainMenu() {
+        System.out.println("  +++++ BATTLESHIP GAME +++++");
+        System.out.println("   +++++  GET STARTED  +++++");
+        System.out.println("     +++++ GAME MODE +++++ \n");
+        System.out.println("     " + PLAYER_VS_PLAYER + ". Player VS Player");
+        System.out.println("     " + PLAYER_VS_COMPUTER + ". Player VS Computer");
+        System.out.println("           " + EXIT_GAME + ". Exit");
+        System.out.println();
+        System.out.print("Enter your choice: ");
+    }
+
+
 }
 
