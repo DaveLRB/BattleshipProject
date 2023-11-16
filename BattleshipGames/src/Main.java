e025035ea2fdb02e5b6580af409332b5752550b8
Fábio <fabio.goncalves@school.mindera.com>
2023-11-16 18:46:27 +0000

    main updated and working

--- BattleshipGames/src/Main.java
+++ BattleshipGames/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Player player2 = new Player();
         GameManager game = new GameManager();
 
-        Scanner input = new Scanner(System.in);
+        //Scanner input = new Scanner(System.in);
         System.out.println("  +++++ BATTLESHIP GAME +++++");
         System.out.println("   +++++  GET STARTED  +++++");
         System.out.println("     +++++ GAME MODE +++++ \n");
@@ -14,25 +14,12 @@ public class Main {
         System.out.println("     2. Player VS Computer");
         System.out.println("           0. Exit");
         System.out.println();
-        String options = input.next();
+        Scanner input = new Scanner(System.in);
+        String options = input.nextLine();
         switch (options) {
             case "1":
-                System.out.println("1. Place Ships Manually\n2. Place Ships Randomly");
-                input.nextLine();
-                String userInput = input.nextLine();
-
-                System.out.println(userInput);
-                switch(userInput){
-                    case "1":
-                        game.placeShipsManually(player1);
-                        break;
-                    case "2":
-                        game.placeShipsRandomly(player1);
-                        break;
-                    default:
-                        System.out.println("please input correct one");
-                        break;
-                }
+                playerPlay(player1,game, "1");
+                playerPlay(player2,game,"2");
                 //game.startPlayerVsPlayerGame();
                 break;
             case "2":
@@ -45,6 +32,35 @@ public class Main {
                 break;
             default:
                 System.out.println("Invalid option, please input correct one");
+                break;
+        }
+
+    }
+
+    private static void playerPlay(Player player, GameManager game, String numberOfPlayer){
+        System.out.println("\nIt's the player"+numberOfPlayer+" time to choose\n");
+        System.out.println("1. Place Ships Manually\n2. Place Ships Randomly");
+        Scanner scan = new Scanner(System.in);
+        String userInputPlayer = scan.nextLine();
+        switch(userInputPlayer){
+            case "1":
+                System.out.println("\nIt's the player"+numberOfPlayer+" time to choose\n");
+                game.placeShipsManually(player);
+                break;
+            case "2":
+                game.placeShipsRandomly(player);
+                String choiceInput;
+                do{
+                    System.out.println("\n1. Continue\n2. Redo randomizer\n");
+                    //input.nextLine();
+                    choiceInput = scan.nextLine();
+                    if(!(choiceInput.equals("1"))){
+                        game.placeShipsRandomly(player);
+                    }
+                }while(!(choiceInput.equals("1")));
+                break;
+            default:
+                System.out.println("please input correct one");
                 break;
         }
     }
